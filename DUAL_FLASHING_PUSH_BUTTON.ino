fa0c18d3fcbd896f010e2fa01c4b185e6236ed4f
ziadbadr <36537915+ziadbadr@users.noreply.github.com>
2018-02-21 20:48:01 +0200

    Add files via upload

---
+++ DUAL_FLASHING_PUSH_BUTTON.ino
@@ -0,0 +1,31 @@
+const int LED1 = 11;
+const int LED2 = 13;
+const int VOLT = 2;
+
+
+void setup() 
+{  
+  pinMode (LED1,OUTPUT);
+  pinMode (LED2,OUTPUT);
+  pinMode (VOLT,INPUT);
+ 
+}
+
+void loop()
+{  
+   if (digitalRead (VOLT)== HIGH)
+
+    { digitalWrite (LED1,HIGH);
+      delay (500);
+      digitalWrite (LED1,LOW);
+      digitalWrite (LED2,HIGH);
+      delay (500);
+      digitalWrite (LED2,LOW);
+    }
+    else 
+    {   digitalWrite (LED1,LOW);
+        digitalWrite (LED2,LOW);
+    }    
+   
+
+}
