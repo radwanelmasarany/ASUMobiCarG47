c5666232b5eec9c38e7adf91c92e13ef5b062c35
ziadbadr <36537915+ziadbadr@users.noreply.github.com>
2018-02-21 18:10:15 +0200

    Add files via upload

---
+++ PULSE_25__duty_cycle.ino
@@ -0,0 +1,13 @@
+const int PULSE = 11;
+void setup()
+{ 
+  pinMode (PULSE,OUTPUT);
+}
+
+void loop() 
+{ 
+  digitalWrite (PULSE,HIGH);
+  delay (500);
+  digitalWrite (PULSE,LOW);
+  delay (1500);
+}
