3829cf298640e1968ca13ee72509610cabef8ec6
ziadbadr <36537915+ziadbadr@users.noreply.github.com>
2018-07-06 23:12:09 +0200

    Add files via upload

---
+++ phase three/codes/Arduino/rectangle function.ino
@@ -0,0 +1,19 @@
+void rectangle ()
+{
+  forward();
+  delay(3000);
+  left();
+  delay(250);
+ forward();
+ delay (2000);
+  left();
+  delay(250);
+  forward ();
+  delay(3000);
+   left();
+  delay(250);
+  forward ();
+  delay (2000);
+  stop()
+}
+
